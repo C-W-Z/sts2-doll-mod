81df9d68507582f27b707e459684f44bf796084a
C-W-Z <cwz.me@proton.me>
2026-05-31 22:57:46 +0800

    restart from RitsuLibTemplate

--- Models/PotionPools/DollPotionPool.cs
+++
@@ -1,9 +0,0 @@
-using BaseLib.Abstracts;
-
-namespace Doll.Models.PotionPools;
-
-public sealed class DollPotionPool : CustomPotionPoolModel
-{
-    // 能量图标。
-    public override string EnergyColorName => "ironclad";
-}
--- Nodes/NDoll.cs
+++
@@ -1,7 +0,0 @@
-using Godot;
-using MegaCrit.Sts2.Core.Nodes.Combat;
-
-namespace Doll.Nodes;
-
-[GlobalClass]
-public partial class NDoll : NCreatureVisuals { }
